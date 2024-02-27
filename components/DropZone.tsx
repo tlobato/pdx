ca62c0e90844d0fe10e2fd3cfcd487614d06b357
tomaslobato <tlobatodev@gmail.com>
2024-02-27 15:09:02 -0300

    &apos corrected

--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { useMutation } from "@tanstack/react-query";
 import Dropzone, { useDropzone } from "react-dropzone";
-import axios from 'axios'
+import axios from "axios";
 
 export default function DropZone() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
   const uploadFile = useMutation({
     mutationFn: async (file: File) => {
-      const formData = new FormData()
-      formData.append('pdf', file)
-      const res = await axios.post('/api/upload', formData)
-      console.log(res)
-      return {res}
-    }
-  })
-  
+      const formData = new FormData();
+      formData.append("pdf", file);
+      const res = await axios.post("/api/upload", formData);
+      console.log(res);
+      return { res };
+    },
+  });
+
   return (
     <Dropzone onDrop={(acceptedFiles) => uploadFile.mutate(acceptedFiles[0])}>
       {({ getRootProps, getInputProps }) => (
@@ -25,7 +25,7 @@ export default function DropZone() {
             className="bg-orange-200 rounded-2xl py-10 px-8 border-2 border-orange-600 border-dashed text-lg font-semibold hover:cursor-pointer"
           >
             <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <p>Drag and drop some files here, or click to select files</p>
           </div>
         </section>
       )}
