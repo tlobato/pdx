768d5326039af713652a801cf67e7568d48d742a
Tomas Lobato <tlobatodev@gmail.com>
2024-02-28 00:01:20 -0300

    clsx not installed

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,7 +1,3 @@
-import { toast } from "sonner";
-import { Button } from "./ui/Button";
-import {  ChevronDownIcon } from "lucide-react";
-
 export default function Chat() {
   return (
     <div className="bg-white rounded-3xl md:rounded-none min-h-screen border border-black md:w-[39%] box-border p-4">
